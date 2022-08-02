03ec2ecaad49b4b27e1ba88134a2ec157b909e9f
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-08-01 21:42:11 -0300

    commit

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -3,13 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 np.random.seed(5)
-fruit = np.random.randint(0, 20, (4,3))
+fruit = np.random.randint(0, 20, (4, 3))
 
 labels = ["Farrah", "Fred", "Felicia"]
 plt.bar(labels, fruit[0], color="r", width=0.5)
-plt.bar(labels, fruit[1], color="yellow", width=0.5, bottom=np.sum(fruit[:1], axis=0))
-plt.bar(labels, fruit[2], color="#ff8000", width=0.5, bottom=np.sum(fruit[:2], axis=0))
-plt.bar(labels, fruit[3], color="#ffe5b4", width=0.5, bottom=np.sum(fruit[:3], axis=0))
+b = [
+    np.sum(fruit[:1], axis=0),
+    np.sum(fruit[:2], axis=0),
+    np.sum(fruit[:3], axis=0)
+]
+plt.bar(labels, fruit[1], color="yellow", width=0.5, bottom=b[0])
+plt.bar(labels, fruit[2], color="#ff8000", width=0.5, bottom=b[1])
+plt.bar(labels, fruit[3], color="#ffe5b4", width=0.5, bottom=b[2])
 plt.legend(["apples", "bananas", "oranges", "peaches"])
 plt.ylim(0, 80)
 plt.title("Number of Fruit per Person")
