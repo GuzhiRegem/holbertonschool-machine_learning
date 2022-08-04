ebf3e898e4f707395bacea027aa7b59e8d0efdfb
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-08-03 22:48:52 -0300

    commit

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,9 +6,11 @@
 
 def poly_integral(poly, C=0):
     """ poly integral """
-    if not type(C) in [int, float] or type(poly) != list:
+    if type(C) != int or type(poly) != list or poly == 0:
         return None
     out = [C]
+    if poly == [0]:
+        return out
     try:
         for i in range(len(poly)):
             v = poly[i] / (i + 1)
