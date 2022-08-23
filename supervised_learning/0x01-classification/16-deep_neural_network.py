543808882d827ab4e068c77af09402ee2415dba7
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-08-22 21:05:34 -0300

    commit

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -15,14 +15,13 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if type(layers) != list:
             raise TypeError("layers must be a list of positive integers")
-        for val in layers:
-            if type(val) != int or val <= 0:
-                raise TypeError("layers must be a list of positive integers")
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
         for lay in range(self.L):
+            if type(val) != int or val <= 0:
+                raise TypeError("layers must be a list of positive integers")
             self.weights[f"b{lay + 1}"] = np.zeros((layers[lay], 1))
             prev = layers[lay - 1] if (lay > 0) else nx
-            self.weights[f"W{lay + 1}"] = np.random.normal(0.0, 1.0, (layers[lay], prev))
+            self.weights[f"W{lay + 1}"] = np.random.randn(layers[lay], prev)
             self.weights[f"W{lay + 1}"] *= np.sqrt(2/prev)
