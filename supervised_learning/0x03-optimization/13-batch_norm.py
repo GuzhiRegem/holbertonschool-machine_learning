e429458139c0d68fb813bb9f850464978efc9a86
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-08-31 22:35:07 -0300

    commit

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def batch_norm(Z, gamma, beta, epsilon):
     """ function """
-    mean = Z.mean(axis=0)
-    var = Z.var(axis=0)
+    mean = np.mean(Z, axis=0)
+    var = np.var(Z, axis=0)
     norm = (Z - mean) / np.sqrt(var + epsilon)
     return (gamma * norm) + beta
--- supervised_learning/0x03-optimization/main.py
+++ supervised_learning/0x03-optimization/main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 import numpy as np
-normalization_constants = __import__('0-norm_constants').normalization_constants
+batch_norm = __import__('13-batch_norm').batch_norm
 
 if __name__ == '__main__':
     np.random.seed(0)
     a = np.random.normal(0, 2, size=(100, 1))
     b = np.random.normal(2, 1, size=(100, 1))
     c = np.random.normal(-3, 10, size=(100, 1))
-    X = np.concatenate((a, b, c), axis=1)
-    m, s = normalization_constants(X)
-    print(m)
-    print(s)
+    Z = np.concatenate((a, b, c), axis=1)
+    gamma = np.random.rand(1, 3)
+    beta = np.random.rand(1, 3)
+    print(Z[:10])
+    Z_norm = batch_norm(Z, gamma, beta, 1e-8)
+    print(Z_norm[:10])
