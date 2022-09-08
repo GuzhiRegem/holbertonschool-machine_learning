53672d509c6a2022dd94776717d44cb77fde33c7
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-09-08 20:54:33 -0300

    commit

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -8,22 +8,13 @@ import numpy as np
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """ l2 gradient """
     m = Y.shape[1]
-    for i in range(L, 0, -1):
-        kw0 = "W" + str(i)
-        kw1 = "W" + str(i + 1)
-        kb = "b" + str(i)
-        A = cache["A" + str(i)]
-        prev = cache["A" + str(i - 1)]
-        if i == L:
-            dZ = A - Y
-            dW = (np.matmul(prev, dZ.T) / m).T
-        else:
-            dW2 = np.matmul(weights[kw1].T, dZ2)
-            tanh = 1 - (A * A)
-            dZ = dW2 * tanh
-            dW = np.matmul(dZ, prev.T) / m
-        dW_L2 = dW + (lambtha / m) * weights[kw0]
+
+    dZ = cache["A" + str(L)] - Y
+    for lay in range(L, 0, -1):
+        A = cache["A" + str(lay - 1)]
         db = np.sum(dZ, axis=1, keepdims=True) / m
-        weights[kw0] -= alpha * dW_L2
-        weights[kb] -= alpha * db
-        dZ2 = dZ
+        dW = np.dot(dZ, A.T) / m
+        dZ = np.dot(weights["W" + str(lay)].T, dZ) * (1 - (A ** 2))
+        dW_L2 = dW + (lambtha / m) * weights["W" + str(lay)]
+        weights["W" + str(lay)] -= dW_L2* alpha
+        weights["b" + str(lay)] -= db * alpha
