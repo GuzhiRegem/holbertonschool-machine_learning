774a57d006ed6517d2bbc175f37b7e2b9ba0b244
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-09-08 21:15:34 -0300

    commit

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -3,4 +3,20 @@
     module
 """
 import numpy as np
-import tensorflow.compat.v1 as tf
+
+
+def dropout_forward_prop(X, weights, L, keep_prob):
+    """ dropout """
+    cache = {"A0": X}
+    for lay in range(1, L + 1):
+        prev = cache["A" + str(lay - 1)]
+        w = weights["W" + str(lay)]
+        b = weights["b" + str(lay)]
+        Z = np.dot(w, prev) + b
+        if lay == L:
+            t = np.exp(Z)
+            A = t / sum(t)
+        else:
+            A = (np.exp(Z)-np.exp(-Z))/(np.exp(Z)+np.exp(-Z))
+        cache["A" + str(lay)] = A
+    return cache
