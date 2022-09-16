95bf2e2f19f8af37328959b8a7749e8765d157f2
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-09-16 17:59:41 -0300

    commit

---
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+    module
+"""
+import numpy as np
+
+
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """ convolve """
+    ks = np.array(kernel.shape)
+    siz = np.array(images.shape[1:3])
+    st = np.array(stride)
+    c = images.shape[3]
+    if padding == "valid":
+        ph, pw = (0, 0)
+    elif padding == "same":
+        ph, pw = ((((siz - 1) * st) + ks - siz) // 2) + 1
+    else:
+        ph, pw = padding
+    img = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                 'constant', constant_values=0)
+    sh = ((siz + (2 * np.array((ph, pw))) - ks[:2]) // st) + 1
+    out = np.zeros(shape=(images.shape[0], sh[0], sh[1]))
+    ite = (siz + (2 * ph) - ks[:2] + 1)
+    for i, x in enumerate(list(range(0, ite[0], st[0]))):
+        for j, y in enumerate(list(range(0, ite[1], st[1]))):
+            sp = img[:, x: x + ks[0], y: y + ks[1], :]
+            out[:, i, j] = np.sum(sp * kernel, axis=(1, 2, 3))
+    return out
--- math/0x04-convolutions_and_pooling/main.py
+++ math/0x04-convolutions_and_pooling/main.py
@@ -2,19 +2,19 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-convolve_grayscale = __import__('3-convolve_grayscale').convolve_grayscale
+convolve_channels = __import__('4-convolve_channels').convolve_channels
 
 
 if __name__ == '__main__':
 
-    dataset = np.load('../../supervised_learning/data/MNIST.npz')
-    images = dataset['X_train']
+    dataset = np.load('../../supervised_learning/data/animals_1.npz')
+    images = dataset['data']
     print(images.shape)
-    kernel = np.array([[1 ,0, -1], [1, 0, -1], [1, 0, -1]])
-    images_conv = convolve_grayscale(images, kernel, padding='valid', stride=(2, 2))
+    kernel = np.array([[[0, 0, 0], [-1, -1, -1], [0, 0, 0]], [[-1, -1, -1], [5, 5, 5], [-1, -1, -1]], [[0, 0, 0], [-1, -1, -1], [0, 0, 0]]])
+    images_conv = convolve_channels(images, kernel, padding='valid')
     print(images_conv.shape)
 
-    plt.imshow(images[0], cmap='gray')
+    plt.imshow(images[0])
     plt.show()
-    plt.imshow(images_conv[0], cmap='gray')
+    plt.imshow(images_conv[0])
     plt.show()
