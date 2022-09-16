419ad6eca0310705fe41a0dca4874dc55864897f
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-09-15 23:45:57 -0300

    commit

---
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+    module
+"""
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    """ convolve """
+    m = images.shape[0]
+    pad = [kernel.shape[0] // 2, kernel.shape[1] // 2]
+    out = np.zeros(shape=(
+        m,
+        images.shape[1] - pad[0] * 2,
+        images.shape[2] - pad[1] * 2
+    ))
+    for x in range(out.shape[1]):
+        for y in range(out.shape[2]):
+            sp = images[:, x: x + 2 + pad[0], y: y + 2 + pad[1]]
+            su = sp * kernel
+            out[:, x, y] = np.sum(su, axis=(1, 2))
+    return out
