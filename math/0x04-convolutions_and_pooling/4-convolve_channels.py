40cdbe4b9f13499313472d8158b19bb0b6c1db33
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-09-16 20:15:00 -0300

    commit

--- math/0x04-convolutions_and_pooling/4-convolve_channels.py
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -18,9 +18,10 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
         ph, pw = padding
     img = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
                  'constant', constant_values=0)
-    sh = ((siz + (2 * np.array((ph, pw))) - ks[:2]) // st) + 1
+    pad = np.array((ph, pw))
+    sh = ((siz + (2 * pad) - ks[:2]) // st) + 1
     out = np.zeros(shape=(images.shape[0], sh[0], sh[1]))
-    ite = (siz + (2 * np.array(ph, pw)) - ks[:2] + 1)
+    ite = (siz + (2 * pad) - ks[:2] + 1)
     for i, x in enumerate(list(range(0, ite[0], st[0]))):
         for j, y in enumerate(list(range(0, ite[1], st[1]))):
             sp = img[:, x: x + ks[0], y: y + ks[1], :]
