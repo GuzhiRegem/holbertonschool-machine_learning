7991aece2ebe07de45b0cadf1da1de48407a4136
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-09-16 00:21:52 -0300

    commit

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -8,11 +8,11 @@ import numpy as np
 def convolve_grayscale_valid(images, kernel):
     """ convolve """
     m = images.shape[0]
-    pad = [kernel.shape[0] // 2, kernel.shape[1] // 2]
+    pad = kernel.shape
     siz = images.shape[1:]
-    out = np.zeros(shape=(m, siz[0] - pad[0] * 2, siz[1] - pad[1] * 2))
+    out = np.zeros(shape=(m, siz[0] - pad[0] + 1, siz[1] - pad[1] + 1))
     for x in range(out.shape[1]):
         for y in range(out.shape[2]):
-            sp = images[:, x: x + 2 + pad[0], y: y + 2 + pad[1]]
-            out[:, x, y] = np.tensordot(sp, kernel)
+            sp = images[:, x: x + pad[0], y: y + pad[1]]
+            out[:, x, y] = np.sum(sp * kernel, axis=(1, 2))
     return out
