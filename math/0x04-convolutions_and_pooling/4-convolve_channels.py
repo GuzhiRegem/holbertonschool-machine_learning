0bdb09121a0065521f1c3a73da42e67a3cfc79ab
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-09-16 20:04:38 -0300

    commit

--- math/0x04-convolutions_and_pooling/4-convolve_channels.py
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -10,11 +10,10 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     ks = np.array(kernel.shape)
     siz = np.array(images.shape[1:3])
     st = np.array(stride)
-    c = images.shape[3]
     if padding == "valid":
         ph, pw = (0, 0)
     elif padding == "same":
-        ph, pw = ((((siz - 1) * st) + ks - siz) // 2) + 1
+        ph, pw = ((((siz - 1) * st) + ks[:2] - siz) // 2) + 1
     else:
         ph, pw = padding
     img = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
