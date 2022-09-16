388c544befc433e6d5d6515c82fb01ae9e3667cf
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-09-16 00:53:53 -0300

    commit

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -7,12 +7,13 @@ import numpy as np
 
 def convolve_grayscale_same(images, kernel):
     """ convolve """
-    pad = np.array(kernel.shape)
-    img = np.pad(images, ((0, 0), pad // 2, pad // 2),
+    ks = np.array(kernel.shape)
+    pad = np.ceil(ks - 1).astype(int)
+    img = np.pad(images, ((0, 0), (pad[0], pad[0]), (pad[1], pad[1])),
                  'constant', constant_values=0)
     out = np.zeros(shape=images.shape)
     for x in range(out.shape[1]):
         for y in range(out.shape[2]):
-            sp = img[:, x: x + pad[0], y: y + pad[1]]
+            sp = img[:, x: x + ks[0], y: y + ks[1]]
             out[:, x, y] = np.sum(sp * kernel, axis=(1, 2))
     return out
--- math/0x04-convolutions_and_pooling/main.py
+++ math/0x04-convolutions_and_pooling/main.py
@@ -10,11 +10,11 @@ if __name__ == '__main__':
     dataset = np.load('../../supervised_learning/data/MNIST.npz')
     images = dataset['X_train']
     print(images.shape)
-    kernel = np.array([[1 ,0, -1, 1], [1, 0, -1, 1], [1, 0, -1, 1], [0, 0, 0, 0]])
+    kernel = np.array([[-1, -1, 1], [-1, 4, -1], [1, -1, -1]])
     images_conv = convolve_grayscale_same(images, kernel)
     print(images_conv.shape)
 
-    plt.imshow(images[0], cmap='gray')
+    plt.imshow(images[1], cmap='gray')
     plt.show()
-    plt.imshow(images_conv[0], cmap='gray')
+    plt.imshow(images_conv[1], cmap='gray')
     plt.show()
