1b8feb6f05ff707b3d1df8813e223839a8359aaa
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-09-21 23:47:05 -0300

    commit

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -4,27 +4,24 @@
 """
 import tensorflow.compat.v1 as tf
 
+
 def lenet5(x, y):
     """ conv forward """
     init = tf.keras.initializers.VarianceScaling(scale=2.0)
-    layers = [
-        tf.layers.Conv2D(filters=6, kernel_size=5, padding='same'),
-        tf.layers.MaxPooling2D(pool_size=2, strides=2),
-        tf.layers.Conv2D(filters=16, kernel_size=5, padding='valid'),
-        tf.layers.MaxPooling2D(pool_size=2, strides=2),
-        tf.layers.Flatten(),
-        tf.layers.Dense(120, activation=tf.nn.relu, kernel_initializer=init),
-        tf.layers.Dense(84, activation=tf.nn.relu, kernel_initializer=init),
-        tf.layers.Dense(10, kernel_initializer=init)
-    ]
-    out = x
-    for lay in layers:
-        out = lay(out)
-    softmax = tf.nn.softmax(out)
-    loss = tf.losses.softmax_cross_entropy(y, logits=out)
+    act = {"activation": tf.nn.relu, "kernel_initializer": init}
+    o = tf.layers.Conv2D(filters=6, kernel_size=5, padding='same', **act)(x)
+    o = tf.layers.MaxPooling2D(pool_size=2, strides=2)(o)
+    o = tf.layers.Conv2D(filters=16, kernel_size=5, padding='valid', **act)(o)
+    o = tf.layers.MaxPooling2D(pool_size=2, strides=2)(o)
+    o = tf.layers.Flatten()(o)
+    o = tf.layers.Dense(120, **act)(o)
+    o = tf.layers.Dense(84, **act)(o)
+    o = tf.layers.Dense(10, kernel_initializer=init)(o)
+    softmax = tf.nn.softmax(o)
+    loss = tf.losses.softmax_cross_entropy(y, logits=o)
     op = tf.train.AdamOptimizer().minimize(loss)
-    y_pred = tf.math.argmax(out, axis=1)
-    y_out = tf.math.argmax(y, axis=1)
-    equality = tf.math.equal(y_pred, y_out)
+    y_pred = tf.argmax(o, axis=1)
+    y_out = tf.argmax(y, axis=1)
+    equality = tf.equal(y_pred, y_out)
     accuracy = tf.reduce_mean(tf.cast(equality, 'float'))
     return softmax, op, loss, accuracy
