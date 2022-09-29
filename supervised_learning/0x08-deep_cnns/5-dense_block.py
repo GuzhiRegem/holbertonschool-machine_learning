576fc30ef08712570b52367b8fd975ab822b8cb1
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-09-29 19:55:20 -0300

    commit

---
+++ supervised_learning/0x08-deep_cnns/5-dense_block.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+    module
+"""
+import tensorflow.keras as K
+
+
+def dense_block(X, nb_filters, growth_rate, layers):
+    """ identity_block """
+    init = K.initializers.he_normal()
+    act = {
+            "kernel_initializer": init,
+            "padding": "same"
+    }
+    L = K.layers
+
+    def ac(x):
+        return L.Activation(K.activations.relu)(x)
+    for lay in range(layers):
+        out = ac(L.BatchNormalization(axis=3)(X))
+        out = L.Conv2D(4 * growth_rate, 1, **act)(out)
+        out = ac(L.BatchNormalization(axis=3)(out))
+        out = L.Conv2D(growth_rate, 3, **act)(out)
+        X = L.concatenate([X, out])
+    return X, nb_filters
--- supervised_learning/0x08-deep_cnns/main.py
+++ supervised_learning/0x08-deep_cnns/main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 import tensorflow.keras as K
-projection_block = __import__('3-projection_block').projection_block
+dense_block = __import__('5-dense_block').dense_block
 
 if __name__ == '__main__':
-    X = K.Input(shape=(224, 224, 3))
-    Y = projection_block(X, [64, 64, 256])
+    X = K.Input(shape=(56, 56, 64))
+    Y, nb_filters = dense_block(X, 64, 32, 6)
     model = K.models.Model(inputs=X, outputs=Y)
     model.summary()
+    print(nb_filters)
