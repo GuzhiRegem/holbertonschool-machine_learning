a2c04f3e70dd2b8a191b186d992cf07a133e5a2b
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-09-29 19:25:17 -0300

    commit

---
+++ supervised_learning/0x08-deep_cnns/3-projection_block.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+    module
+"""
+import tensorflow.keras as K
+
+
+def projection_block(A_prev, filters, s=2):
+    """ identity_block """
+    init = K.initializers.he_normal()
+    act = {
+            "kernel_initializer": init,
+            "padding": "same"
+    }
+    L = K.layers
+
+    def ac(x):
+        return L.Activation(K.activations.relu)(x)
+    out = L.Conv2D(filters[0], 1, strides=s,  **act)(A_prev)
+    out = ac(L.BatchNormalization(axis=3)(out))
+    out = L.Conv2D(filters[1], 3, **act)(out)
+    out = ac(L.BatchNormalization(axis=3)(out))
+    out = L.Conv2D(filters[2], 1, **act)(out)
+    out = L.BatchNormalization(axis=3)(out)
+    inp = L.Conv2D(filters[2], 1, strides=s, **act)(A_prev)
+    inp = ac(L.BatchNormalization(axis=3)(inp))
+    return ac(L.Add()([out, inp]))
