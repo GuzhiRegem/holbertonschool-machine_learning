8906c3da10a7f642959f106aec1bb10bfa03af6b
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-09-29 22:25:08 -0300

    commit

---
+++ supervised_learning/0x08-deep_cnns/4-resnet50.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+    module
+"""
+import tensorflow.keras as K
+identity_block = __import__('2-identity_block').identity_block
+projection_block = __import__('3-projection_block').projection_block
+
+
+def resnet50():
+    """ inception_network """
+    init = K.initializers.he_normal()
+    act = {"kernel_initializer": init, "padding": "same"}
+    L = K.layers
+    def block(lis, prev, amount, s=2):
+        o = projection_block(prev, lis, s)
+        for i in range(amount - 1):
+            o = identity_block(o, lis)
+        return o
+
+    X = K.Input(shape=(224, 224, 3))
+    out = L.Conv2D(64, 7, strides=2, **act)(X)
+    out = L.BatchNormalization(axis=3)(out)
+    out = L.Activation(K.activations.relu)(out)
+    out = L.MaxPooling2D(pool_size=3, strides=2, padding="same")(out)
+    out = block([64, 64, 256], out, 3, 1)
+    out = block([128, 128, 512], out, 4)
+    out = block([256, 256, 1024], out, 6)
+    out = block([512, 512, 2048], out, 3)
+    out = L.AveragePooling2D(7, 1, padding='valid')(out)
+    out = L.Dense(1000, activation='softmax', kernel_initializer=init)(out)
+
+    return K.models.Model(inputs=X, outputs=out)
--- supervised_learning/0x08-deep_cnns/main.py
+++ supervised_learning/0x08-deep_cnns/main.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 
 import tensorflow.keras as K
-transition_layer = __import__('6-transition_layer').transition_layer
+resnet50 = __import__('4-resnet50').resnet50
 
 if __name__ == '__main__':
-    X = K.Input(shape=(56, 56, 256))
-    Y, nb_filters = transition_layer(X, 256, 0.5)
-    model = K.models.Model(inputs=X, outputs=Y)
+    model = resnet50()
     model.summary()
-    print(nb_filters)
