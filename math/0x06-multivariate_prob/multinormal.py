48bfeb7351ed597957a2fb93835671c1229324eb
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-11-07 13:16:17 -0300

    commit

---
+++ math/0x06-multivariate_prob/multinormal.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    module
+"""
+import numpy as np
+
+
+class MultiNormal:
+    """ multinormal """
+    def __init__(self, data):
+        if type(data) is not np.ndarray or len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        n, d = data.shape
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
+        self.mean = np.mean(data, axis=0, keepdims=True)
+        self.cov = np.dot(data.T - mean.T, data - mean) / (n - 1)
