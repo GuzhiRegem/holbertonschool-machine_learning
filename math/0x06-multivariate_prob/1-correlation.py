0ebc653264d0376f6859329d199e5af52a5420cb
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-11-07 13:11:48 -0300

    commit

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -8,8 +8,8 @@ import numpy as np
 def correlation(C):
     """ correlation """
     if type(C) is not np.ndarray:
-        raise TypeError("X must be a 2D numpy.ndarrayy")
+        raise TypeError("X must be a 2D numpy.ndarray")
     if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
-        raise ValueError("C must be a 2D square matrix")
+        raise ValueError("C must be a 2D square matrixx")
     std = np.sqrt(np.diag(C))
     return C / np.outer(std, std)
