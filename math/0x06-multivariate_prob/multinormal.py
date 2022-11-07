01c3c65e842452fd22218bc3f75d4ef375960d36
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-11-07 13:40:31 -0300

    commit

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -15,3 +15,16 @@ class MultiNormal:
             raise ValueError("data must contain multiple data points")
         self.mean = np.mean(data, axis=1, keepdims=True)
         self.cov = np.dot(data - self.mean, data.T - self.mean.T) / (n - 1)
+    
+    def pdf(self, x):
+        """ pdf """
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.cov.shape[0]
+        if len(x.shape) != 2 or x.shape[0] != d or x.shape[1] != 1:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        inv = np.linalg.inv(self.cov)
+        mul = np.dot(np.dot((x - self.mean).T, inv), (x - self.mean))
+        pdf = np.exp(-0.5 * mul)
+        pdf /= np.sqrt(((2 * np.pi) ** d) * np.linalg.det(self.cov))
+        return pdf[0][0]
