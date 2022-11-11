3d622c3face2280a08ad632ef158c2e21555ae81
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-11-10 23:43:20 -0300

    commit

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -5,7 +5,7 @@
 import numpy as np
 
 
-def likelihood(x, n, P):
+def intersection(x, n, P, Pr):
     """ likelihood """
     if (type(n) != int) or (n <= 0):
         raise ValueError("n must be a positive integer")
@@ -17,7 +17,12 @@ def likelihood(x, n, P):
         raise ValueError("x cannot be greater than n")
     if (type(P) != np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
+    if (type(Pr) != np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     for val in P:
         if val < 0 or val > 1:
             raise ValueError("All values in P must be in the range [0, 1]")
+    for val in Pr:
+        if val < 0 or val > 1:
+            raise ValueError("All values in Pr must be in the range [0, 1]")
     return np.array([0.5, 0.5])
