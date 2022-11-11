3235fc8f4415e91abfc0292d1278c038156b38c2
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-11-10 23:37:55 -0300

    commit

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -10,7 +10,9 @@ def likelihood(x, n, P):
     if (type(n) != int) or (n <= 0):
         raise ValueError("n must be a positive integer")
     if (type(x) != int) or (x < 0):
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is " + "greater than or equal to 0"
+        )
     if x > n:
         raise ValueError("x cannot be greater than n")
     if (type(P) != np.ndarray) or P.shape != 1:
