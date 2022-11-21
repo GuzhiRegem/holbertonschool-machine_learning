c362181effd3888dd6e75b74ad8ea896ec661f89
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-11-21 15:45:11 -0300

    commit

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -7,7 +7,11 @@ import numpy as np
 
 def initialize(X, k):
     """ initialize """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+    if type(k) is not int or k <= 0:
+        return None
     n, d = X.shape
-    a_min = X.min()
-    a_max = X.max()
+    a_min = np.min(X, 0)
+    a_max = np.max(X, 0)
     return np.random.uniform(a_min, a_max, size=(k, d))
