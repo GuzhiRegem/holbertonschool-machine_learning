66826af96533c64cfba881f3315a0284891d482b
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-11-24 16:16:17 -0300

    absorbing

--- unsupervised_learning/0x02-hmm/2-absorbing.py
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -7,4 +7,10 @@ import numpy as np
 
 def absorbing(P):
     """ markov_chain regular """
-    return False
+    if type(P) is not np.ndarray or len(P.shape) != 2:
+        return False
+    # save value of n and check that P is square
+    n, n_check = P.shape
+    if n != n_check:
+        return False
+    return True
