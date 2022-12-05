ff0327d1e8a0f5480922c092bdaaee3816891896
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-12-05 18:16:26 -0300

    commit

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -13,9 +13,22 @@ class GaussianProcess:
         self.Y = Y_init
         self.l = l
         self.sigma_f = sigma_f
-        self.K = 0
+        self.K = self.kernel(X_init, X_init)
     
     def kernel(self, X1, X2):
         """ kernel """
-        dis = np.linalg.norm(X1 - X2, keepdims=True)
-        return np.exp(dis / (2 * self.l))
+        if type(X1) is not np.ndarray or len(X1.shape) != 2:
+            raise TypeError("X1 must be numpy.ndarray of shape (m, 1)")
+        m, one = X1.shape
+        if one != 1:
+            raise TypeError("X1 must be numpy.ndarray of shape (m, 1)")
+        if type(X2) is not np.ndarray or len(X2.shape) != 2:
+            raise TypeError("X2 must be numpy.ndarray of shape (n, 1)")
+        n, one = X2.shape
+        if one != 1:
+            raise TypeError("X2 must be numpy.ndarray of shape (n, 1)")
+        X1_sum = np.sum(X1 ** 2, 1).reshape(-1, 1)
+        X2_sum = np.sum(X2 ** 2, 1)
+        dis = X1_sum + X2_sum - 2 * np.dot(X1, X2.T)
+        cov = (self.sigma_f ** 2) * np.exp(-0.5 / (self.l ** 2) * dis)
+        return cov 
