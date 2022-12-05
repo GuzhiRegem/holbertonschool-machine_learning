1379c7bcaab640757df5890536250193e488ce3d
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-12-05 18:18:05 -0300

    pycodestyle

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -14,7 +14,7 @@ class GaussianProcess:
         self.l = l
         self.sigma_f = sigma_f
         self.K = self.kernel(X_init, X_init)
-    
+
     def kernel(self, X1, X2):
         """ kernel """
         if type(X1) is not np.ndarray or len(X1.shape) != 2:
@@ -31,4 +31,4 @@ class GaussianProcess:
         X2_sum = np.sum(X2 ** 2, 1)
         dis = X1_sum + X2_sum - 2 * np.dot(X1, X2.T)
         cov = (self.sigma_f ** 2) * np.exp(-0.5 / (self.l ** 2) * dis)
-        return cov 
+        return cov
