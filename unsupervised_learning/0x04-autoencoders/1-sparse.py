65fdd3e9f76158820b669e71b12e17f199c76b4a
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-12-08 08:54:20 -0500

    commit

--- unsupervised_learning/0x04-autoencoders/1-sparse.py
+++ unsupervised_learning/0x04-autoencoders/1-sparse.py
@@ -7,4 +7,29 @@ import tensorflow.keras as keras
 
 def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     """ autoencoder """
-    return None, None, None
+    K, L = keras, keras.layers
+    inputs = {
+        "encod": K.Input(shape=(input_dims,)),
+        "decod": K.Input(shape=(latent_dims,))
+    }
+
+    out = inputs["encod"]
+    for val in hidden_layers:
+        out = L.Dense(val, activation="relu")(out)
+    L1 = K.regularizers.l1(lambtha)
+    out = L.Dense(latent_dims, activation="relu", activity_regularizer=L1)(out)
+    encoder = K.Model(inputs=inputs["encod"], outputs=out)
+
+    out = inputs["decod"]
+    for val in reversed(hidden_layers):
+        out = L.Dense(val, activation="relu")(out)
+    out = L.Dense(input_dims, activation="sigmoid")(out)
+    decoder = K.Model(inputs=inputs["decod"], outputs=out)
+
+    auto = K.Model(
+        inputs=inputs["encod"],
+        outputs=decoder(encoder(inputs["encod"]))
+    )
+    auto.compile(optimizer="adam", loss="binary_crossentropy")
+
+    return encoder, decoder, auto
--- unsupervised_learning/0x04-autoencoders/2-convolutional.py
+++ unsupervised_learning/0x04-autoencoders/2-convolutional.py
@@ -7,4 +7,28 @@ import tensorflow.keras as keras
 
 def autoencoder(input_dims, filters, latent_dims):
     """ autoencoder """
-    return None, None, None
+    K, L = keras, keras.layers
+    inputs = {
+        "encod": K.Input(shape=(input_dims,)),
+        "decod": K.Input(shape=(latent_dims,))
+    }
+
+    out = inputs["encod"]
+    for val in hidden_layers:
+        out = L.Dense(val, activation="relu")(out)
+    out = L.Dense(latent_dims, activation="relu")(out)
+    encoder = K.Model(inputs=inputs["encod"], outputs=out)
+
+    out = inputs["decod"]
+    for val in reversed(hidden_layers):
+        out = L.Dense(val, activation="relu")(out)
+    out = L.Dense(input_dims, activation="sigmoid")(out)
+    decoder = K.Model(inputs=inputs["decod"], outputs=out)
+
+    auto = K.Model(
+        inputs=inputs["encod"],
+        outputs=decoder(encoder(inputs["encod"]))
+    )
+    auto.compile(optimizer="adam", loss="binary_crossentropy")
+
+    return encoder, decoder, auto
