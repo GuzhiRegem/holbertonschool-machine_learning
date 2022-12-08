4710cecec55505f14c8665021a8dd1515adea56c
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-12-07 19:04:44 -0500

    base

---
+++ unsupervised_learning/0x04-autoencoders/2-convolutional.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    module
+"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, filters, latent_dims):
+    """ autoencoder """
+    return None, None, None
