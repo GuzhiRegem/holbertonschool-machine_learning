00d6a8a449a9f236465b646017b7c0c54414b327
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-12-08 09:28:04 -0500

    commit

--- unsupervised_learning/0x04-autoencoders/2-convolutional.py
+++ unsupervised_learning/0x04-autoencoders/2-convolutional.py
@@ -9,28 +9,44 @@ def autoencoder(input_dims, filters, latent_dims):
     """ autoencoder """
     K, L = keras, keras.layers
     inputs = {
-        "encod": K.Input(shape=(input_dims,)),
-        "decod": K.Input(shape=(latent_dims,))
-    }
-    conv_conf = {
-        "activation": "relu",
-        "kernel_size": (3, 3),
-        "padding": "same"
+        "encod": K.Input(shape=(input_dims)),
+        "decod": K.Input(shape=(latent_dims))
     }
 
     out = inputs["encod"]
     for val in filters:
-        out = L.Conv2D(val, **conv_conf)(out)
-        out = L.MaxPooling2D((2, 2), padding="same")(out)
-    encoder = K.Model(inputs=inputs["encod"], outputs=out)
+        out = L.Conv2D(
+            val,
+            activation='relu',
+            kernel_size=(3, 3),
+            padding='same'
+        )(out)
+        out = L.MaxPooling2D((2, 2), padding='same')(out)
+    encoder_outputs = out
+    encoder = K.Model(inputs=inputs["encod"], outputs=encoder_outputs)
 
     out = inputs["decod"]
-    for val in reversed(filters):
-        out = L.Conv2D(val, **conv_conf)(out)
+    for val in reversed(filters[1:]):
+        out = L.Conv2D(
+            val,
+            activation='relu',
+            kernel_size=(3, 3),
+            padding='same'
+        )(out)
         out = L.UpSampling2D((2, 2))(out)
-    out = L.Conv2D(filters[0], **conv_conf, padding="same")(out)
+    out = L.Conv2D(
+        filters[0],
+        kernel_size=(3, 3),
+        padding='valid',
+        activation='relu'
+    )(out)
     out = L.UpSampling2D((2, 2))(out)
-    out = L.Conv2D(input_dims[2], **conv_conf, activation="sigmoid")(out)
+    out = L.Conv2D(
+        input_dims[2],
+        activation='sigmoid',
+        kernel_size=(3, 3),
+        padding='same'
+    )(out)
     decoder = K.Model(inputs=inputs["decod"], outputs=out)
 
     auto = K.Model(
