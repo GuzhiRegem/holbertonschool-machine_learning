be9bd7f8e57701d2306b2c2c829ac4b166eaaa56
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-12-08 09:35:22 -0500

    commit

--- unsupervised_learning/0x04-autoencoders/3-variational.py
+++ unsupervised_learning/0x04-autoencoders/3-variational.py
@@ -9,20 +9,28 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     """ autoencoder """
     K, L = keras, keras.layers
     inputs = {
-        "encod": K.Input(shape=(input_dims,)),
-        "decod": K.Input(shape=(latent_dims,))
+        "encod": K.Input(shape=(input_dims)),
+        "decod": K.Input(shape=(latent_dims))
     }
 
     out = inputs["encod"]
     for val in hidden_layers:
-        out = L.Dense(val, activation="relu")(out)
-    out = L.Dense(latent_dims, activation="relu")(out)
+        out = L.Conv2D(
+            val,
+            activation='relu',
+            kernel_size=(3, 3),
+            padding='same'
+        )(out)
+        out = L.BatchNormalization()(out)
+    out = L.Flatten()(out)
+    out = L.Dense(activation='relu')(out)
+    out = L.BatchNormalization()(out)
     encoder = K.Model(inputs=inputs["encod"], outputs=out)
 
     out = inputs["decod"]
     for val in reversed(hidden_layers):
-        out = L.Dense(val, activation="relu")(out)
-    out = L.Dense(input_dims, activation="sigmoid")(out)
+        out = L.Dense(val, activation='relu')(out)
+    L.Dense(input_dims, activation='sigmoid')
     decoder = K.Model(inputs=inputs["decod"], outputs=out)
 
     auto = K.Model(
