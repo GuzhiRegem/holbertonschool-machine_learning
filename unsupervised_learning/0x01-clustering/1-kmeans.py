4e90f659af055a5e35413f9f6c18566e55334332
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-12-14 15:52:28 -0300

    commit

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -4,12 +4,6 @@
 """
 import numpy as np
 
-
-def get_clusters(X, C):
-    clus = C[:, np.newaxis]
-    return np.argmin(np.sqrt(np.sum(np.square(X - clus), axis=2)), axis=0)
-
-
 def kmeans(X, k, iterations=1000):
     n, d = X.shape
     min_vals = np.min(X, axis=0)
@@ -17,12 +11,12 @@ def kmeans(X, k, iterations=1000):
     C = np.random.uniform(low=min_vals, high=max_vals, size=(k, d))
     for _ in range(iterations):
         clss = np.argmin(np.sum((X[:, None, :] - C)**2, axis=2), axis=1)
-        C_new = np.array([
-            X[clss == c].mean(axis=0)
-            if np.any(clss == c)
-            else min_vals + (max_vals - min_vals) * np.random.random(d)
-            for c in range(k)
-        ])
+        C_new = np.zeros(shape=(k, X.shape[1]))
+        for c in range(k):
+            if not len(X[clss == c]):
+                C_new[c] = np.random.uniform(low=min_vals, high=max_vals)
+            else:
+                C_new[c] = X[clss == c].mean(axis=0)
         if np.all(C == C_new):
             break
         C = C_new
