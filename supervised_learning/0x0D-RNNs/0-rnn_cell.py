7a88f0cf5108849508d633693c9aedf5a0e67e82
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-12-14 14:40:31 -0300

    commit

--- supervised_learning/0x0D-RNNs/0-rnn_cell.py
+++ supervised_learning/0x0D-RNNs/0-rnn_cell.py
@@ -5,18 +5,22 @@
 import numpy as np
 
 class RNNCell:
+    """ RNNCell """
     def __init__(self, i, h, o):
+        """ init """
         self.Wh = np.random.normal(size=(h+i, h))
         self.Wy = np.random.normal(size=(h, o))
         self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
+        """ forward """
         concat = np.concatenate((h_prev, x_t), axis=1)
         h_next = np.tanh(np.dot(concat, self.Wh) + self.bh)
         y = self.softmax(np.dot(h_next, self.Wy) + self.by)
         return h_next, y
 
     def softmax(self, x):
+        """ softmax """
         e_x = np.exp(x - np.max(x))
         return e_x / e_x.sum()
