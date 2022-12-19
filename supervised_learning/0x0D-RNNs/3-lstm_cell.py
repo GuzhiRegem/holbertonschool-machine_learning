c7e6780d9c30e2010ac313160996e09e84662ab4
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-12-19 13:01:49 -0300

    commit

--- supervised_learning/0x0D-RNNs/3-lstm_cell.py
+++ supervised_learning/0x0D-RNNs/3-lstm_cell.py
@@ -5,44 +5,35 @@
 import numpy as np
 
 
-def sigmoid(x):
-    """ sigmoid """
-    return 1 / (1 + np.exp(-x))
-
-
-def softmax(x):
-    """ softmax """
-    exp_x = np.exp(x)
-    return exp_x / np.sum(exp_x, axis=1, keepdims=True)
+def sigmoid(var):
+    """Do sigmoid on var"""
+    ex = np.exp(var)
+    return ex / (1 + ex)
 
 
 class LSTMCell:
     """ LSTMCell """
     def __init__(self, i, h, o):
         """ init """
-        self.Wf = np.random.randn(h, i+h)
-        self.Wu = np.random.randn(h, i+h)
-        self.Wc = np.random.randn(h, i+h)
-        self.Wo = np.random.randn(h, i+h)
-        self.Wy = np.random.randn(o, h)
+        self.Wf = np.random.normal(size=(i + h, h))
+        self.Wu = np.random.normal(size=(i + h, h))
+        self.Wc = np.random.normal(size=(i + h, h))
+        self.Wo = np.random.normal(size=(i + h, h))
+        self.Wy = np.random.normal(size=(h, o))
         self.bf = np.zeros((1, h))
         self.bu = np.zeros((1, h))
         self.bc = np.zeros((1, h))
         self.bo = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
-    def sigmoid(self, x):
-        """ sigmoid """
-        return 1 / (1 + np.exp(-x))
-
     def forward(self, h_prev, c_prev, x_t):
         """ forward """
-        concat = np.concatenate((h_prev, x_t), axis=1)
-        f = self.sigmoid(np.dot(self.Wf, concat) + self.bf)
-        u = self.sigmoid(np.dot(self.Wu, concat) + self.bu)
-        c_bar = np.tanh(np.dot(self.Wc, concat) + self.bc)
-        c_next = f * c_prev + u * c_bar
-        o = self.sigmoid(np.dot(self.Wo, concat) + self.bo)
-        h_next = o * np.tanh(c_next)
-        y = softmax(np.dot(self.Wy, h_next) + self.by)
-        return h_next, c_next, y
+        concat = np.concatenate((h_prev, x_t), 1)
+        forgetsig = sigmoid(np.matmul(concat, self.Wf) + self.bf)
+        middlesig = sigmoid(np.matmul(concat, self.Wu) + self.bu)
+        active = np.tanh(np.matmul(concat, self.Wc) + self.bc)
+        outsig = sigmoid(np.matmul(concat, self.Wo) + self.bo)
+        nextc = forgetsig * c_prev + middlesig * active
+        nexth = outsig * np.tanh(nextc)
+        out = np.exp(np.matmul(nexth, self.Wy) + self.by)
+        return nexth, nextc, out / out.sum(axis=1, keepdims=True)
