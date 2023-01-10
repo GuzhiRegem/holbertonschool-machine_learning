12a708767f84ec6c845804fed58a9b2b7a2aa174
GuzhiRegem <sebastian.moreiradl@gmail.com>
2023-01-10 18:51:54 -0300

    commit

--- supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -2,9 +2,17 @@
 """
     module
 """
-import numpy as np
+from tensorflow.keras.layers import Embedding
 
 
 def gensim_to_keras(model):
     """ bag_of_words """
-    return model.wv.get_keras_embedding(train_embeddings=True)
+    keyed_vectors = model.wv
+    weights = keyed_vectors.vectors
+    index_to_key = keyed_vectors.index_to_key 
+    return Embedding(
+        input_dim=weights.shape[0],
+        output_dim=weights.shape[1],
+        weights=[weights],
+        trainable=True,
+    )
--- supervised_learning/0x0F-word_embeddings/main.py
+++ supervised_learning/0x0F-word_embeddings/main.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
 
-bag_of_words = __import__('0-bag_of_words').bag_of_words
+from gensim.test.utils import common_texts
+word2vec_model = __import__('2-word2vec').word2vec_model
+gensim_to_keras = __import__('3-gensim_to_keras').gensim_to_keras
 
-sentences = ["Holberton school is Awesome!",
-             "Machine learning is awesome",
-             "NLP is the future!",
-             "The children are our future",
-             "Our children's children are our grandchildren",
-             "The cake was not very good",
-             "No one said that the cake was not very good",
-             "Life is beautiful"]
-E, F = bag_of_words(sentences)
-print(E)
-print(F)
+print(common_texts[:2])
+w2v = word2vec_model(common_texts, min_count=1)
+print(gensim_to_keras(w2v))
